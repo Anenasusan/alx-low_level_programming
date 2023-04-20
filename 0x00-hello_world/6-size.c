2416a9191dd29d11b1896df0bd404ec014bf7d19
Anena Susan <anenasusan3@gmail.com>
2023-04-20 12:27:51 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#inlude <stdio.h>
+/**
+ * main -A  program that prints the size of various computer types
+ * Return 0(success)
+ */
+int main(void)
+}
+        char a;
+	int b;
+	long int c;
+	long long int d;	printf("Size of a char: %lu byte(s)\n"; (long)si
+			a));
