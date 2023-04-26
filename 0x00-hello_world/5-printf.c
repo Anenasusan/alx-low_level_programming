df030c63ac194d984e3f7542b5e0bc28f726a4c6
Anena Susan <anenasusan3@gmail.com>
2023-04-26 09:17:08 -0700

    0x00-hello_world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
-/**
- * main - A program that prints a line with printf function
- * Return: 0 (success)
- */
-int main(void)
-{
-puts("\"with proper grammar, but the outcome is a piece of art");
-return (0);
-}
+#incude stdio
