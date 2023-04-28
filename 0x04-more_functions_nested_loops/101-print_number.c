4d8ebef6e0cb11db8899b7fb22e13a9c1edc408f
Anena Susan <anenasusan3@gmail.com>
2023-04-28 06:07:06 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - print an integer
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+	if (n < 0)
+	{
+		n1 = -n;
+		-putchar('-');
+	} else 
+	{
+		nl = n;
+	{
+	if (nl / 10)
+	
+	{
+		print_number(nl / 10);
+	_putchar((nl % 10) + '0');
+}
