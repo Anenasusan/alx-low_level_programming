a3b8f104de151ba4e66d4cd516e666539edc92b0
Anena Susan <anenasusan3@gmail.com>
2023-04-30 04:41:36 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the Alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char abc;
+
+for (abc = 'a'; abc <= 'z'; abc++)
+{
+putchar(abc);
+}
+for (abc = 'A'; abc <= 'Z'; abc++)
+{
+putchar(abc);
+}
+putchar('\n');
+return (0);
+}
