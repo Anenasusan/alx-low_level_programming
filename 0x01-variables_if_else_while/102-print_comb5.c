a2be9b5a4ddfe069b708b6d988590c2775b4438a
Anena Susan <anenasusan3@gmail.com>
2023-04-30 07:18:39 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -  Prints all possible combinations of two two-digit numbers.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 0;
+
+	while (c <= 99)
+	{
+		putchar(c / 10 + '0');
+		putchar(c % 10 + '0');
+		if (c != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+putchar('\n');
+return (0);
+}
