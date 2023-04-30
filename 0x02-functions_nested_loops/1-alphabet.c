efe14def0cdc429cc037ad6036cbbdebacbdba50
Anena Susan <anenasusan3@gmail.com>
2023-04-30 12:57:08 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet- Function to print Alphatet in lowercase,
+ * followed by a line
+ */
+
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+	_putchar ('\n');
+}
