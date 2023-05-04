ba38bf5bef1630fd3c90ecf55cdfea7977920a79
Anena Susan <anenasusan3@gmail.com>
2023-05-04 02:31:54 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* print_number - A function that prints an integer.
-*@n: Integer to be printed
+* print_number - prints an integer
+*@n:integer to be printed
 *
 */
 void print_number(int n)
